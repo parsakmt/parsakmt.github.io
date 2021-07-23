d182a2a8317abae89267514d0bf164dc6afe609d
Parsa Karamat <pkmtyp@gmail.com>
2021-07-23 01:03:12 -0700

    Portfolio Website Commit

---
+++ hamburger.js
@@ -0,0 +1,40 @@
+const navListWrapper = document.querySelector(".nav-list-wrapper"); 
+const hamburgerOpen = document.querySelector(".fas.fa-bars");
+const hamburgerClose = document.querySelector(".far.fa-window-close"); 
+
+var timer;
+
+window.addEventListener("resize", function(event) {
+    clearTimeout(doit);
+    doit = setTimeout(hideOpenAndCloseIcons, 25);
+})
+
+hamburgerOpen.addEventListener('click', openHamburger);
+hamburgerClose.addEventListener('click', closeHamburger);
+
+function openHamburger() {
+    navListWrapper.style.top = '40px'; 
+    hamburgerOpen.style.display = 'none'; 
+    hamburgerClose.style.display = 'block'; 
+}
+
+function closeHamburger() {
+    navListWrapper.style.top = '-100vh'; 
+    hamburgerOpen.style.display = 'block'; 
+    hamburgerClose.style.display = 'none'; 
+}
+
+function hideOpenAndCloseIcons(){
+    if (document.body.clientWidth > 800){
+        hamburgerOpen.style.display = 'none'; 
+        hamburgerClose.style.display = 'none'; 
+    }
+    else if (navListWrapper.style.top == '40px'){
+        hamburgerOpen.style.display = 'none'; 
+        hamburgerClose.style.display = 'block'; 
+    }
+    else{
+        hamburgerOpen.style.display = 'block'; 
+        hamburgerClose.style.display = 'none'; 
+    }
+}
